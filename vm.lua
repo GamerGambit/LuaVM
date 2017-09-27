53520dcc4a7a4afc9019d896d81169d0a357323f
Tyler Duffus <tylerduffus@gmail.com>
2017-09-27 21:24:35 +1000

    Fixed: BRT and BRF not popping from the stack.

--- vm.lua
+++ vm.lua
@@ -167,14 +167,14 @@ local vm = {
 			self.ip = func.bytecode[op]
 		
 		elseif (opcode == opcodes.BRT) then
-			if (self.stack[#self.stack] == true) then
+			if (table.remove(self.stack) == true) then
 				self.ip = func.bytecode[op]
 			else
 				self.ip = self.ip + 1
 			end
 		
 		elseif (opcode == opcodes.BRF) then
-			if (self.stack[#self.stack] == false) then
+			if (table.remove(self.stack) == false) then
 				self.ip = func.bytecode[op]
 			else
 				self.ip = self.ip + 1
