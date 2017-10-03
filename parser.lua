429349b63da6e1a2a5d0f00d420fa549b9e46a7a
Tyler Duffus <tylerduffus@gmail.com>
2017-10-03 11:18:20 +1000

    Added: Precedence 14 expression parsing.

--- parser.lua
+++ parser.lua
@@ -472,6 +472,22 @@ local parser = {
 					end
 				end
 
+				if (expr == nil and precedence < 14 and self.currentToken.contents == "||") then
+					if (prevExpr == nil) then
+						error("[Parser] expected expression")
+					else
+						self:next()
+
+						local res = self:parseExpression(0, nil, closeParenTreatment)
+						if (not res.success) then
+							error("[Parser] expected expression")
+						else
+							expr = newBinaryOperator("||", prevExpr, res.data)
+							exprPrecedence = 13
+						end
+					end
+				end
+
 				if (expr == nil) then
 					return {success = false}
 				end
