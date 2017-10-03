d3687475d0c6f765de3a23af1135a546957ae52a
Tyler Duffus <tylerduffus@gmail.com>
2017-10-03 10:54:17 +1000

    Added: Precedence 7 expression parsing.

--- parser.lua
+++ parser.lua
@@ -355,6 +355,25 @@ local parser = {
 					end
 				end
 
+				if (expr == nil and precedence < 7 and (
+					self.currentToken.contents == '>' or self.currentToken.contents == '<' or
+					self.currentToken.contents == ">=" or self.currentToken.contents == "<=")) then
+					if (prevExpr == nil) then
+						error("[Parser] expected expression")
+					else
+						local currentContents = self.currentToken.contents
+						self:next()
+
+						local res = self:parseExpression(0, nil, closeParenTreatment)
+						if (not res.success) then
+							error("[Parser] expected expression ")
+						else
+							expr = newBinaryOperator(currentContents, prevExpr, res.data)
+							exprPrecedence = 6
+						end
+					end
+				end
+
 				if (expr == nil) then
 					return {success = false}
 				end
